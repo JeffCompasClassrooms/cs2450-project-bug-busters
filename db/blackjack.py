463954955f8f58b2f87dfc4dfd7f54fb50fd5d51
JustDaave <cheetahh2019@gmail.com>
2025-04-03 17:34:55 -0600

    workflow in my branch test

--- db/blackjack.py
+++ db/blackjack.py
@@ -7,7 +7,7 @@ from db import posts, users, helpers
 
 blackjack_bp = Blueprint('blackjack', __name__, url_prefix='/blackjack')
 
-card_categories = ['Hearts', 'Diamonds', 'Clubs', 'Spades'] # Hello
+card_categories = ['Hearts', 'Diamonds', 'Clubs', 'Spades'] # bye
 cards_list = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
 def create_deck():
