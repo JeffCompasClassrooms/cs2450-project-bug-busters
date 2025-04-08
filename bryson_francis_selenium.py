616d60bc3d670585367579c7208f85db9c4bde6c
Bryson Francis <brysonafrancis@gmail.com>
2025-04-08 13:07:32 -0600

    my selenium update

--- bryson_francis_selenium.py
+++ bryson_francis_selenium.py
@@ -1,38 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
-'''
-ELEMENTS EXIST
-ELEMENTS CONTAIN CERTAIN TEXT
-PUT SOMETHING IN A FIELD
-    AND CLICK A BUTTON
-        AND CHECK THAT SOMETHING HAPPENED
+import os
 
-CHECK IF AN ELEMENT IS A CERTAIN COLOR
-'''
-# Specify the path to ChromeDriver
-chrome_driver_path = "/usr/local/bin/chromedriver" #you'll need to put the path to YOUR chromedriver here
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service)
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+
+selenium_url = os.environ.get("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+driver = webdriver.Remote(
+    command_executor=selenium_url,
+    options=options,
+    desired_capabilities=DesiredCapabilities.CHROME
+)
+
+
+from selenium.webdriver.chrome.service import Service
+
+
+# GitHub Actions provides Chrome and chromedriver by default
+service = Service()
+driver = webdriver.Chrome(service=service, options=options)
+
 
 try:
     driver.get("http://localhost:5000/loginscreen")
     time.sleep(2)
 
+
     print("--= Beginning Tests - <Bryson Francis> =--")
     login_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Login']")
+
 
     if login_button:
         print("[PASSED] - Login Button Exists.")
     else:
         print("[FAILED] - Login button not found.")
 
+
     create_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value='Create']")                                                                                                                                          
     if create_button:
         print("[PASSED] - Create Button Exists.")
     else:
         print("[FAILED] - Create button not found.")
+
 
     driver.find_element(By.NAME, "username").send_keys("testuser")
     driver.find_element(By.NAME, "password").send_keys("pass1234")
@@ -41,22 +57,26 @@ try:
     time.sleep(5)
 
 
+
+
     driver.find_element(By.CSS_SELECTOR, "input[type='text'][name='name']").send_keys("goose")
     driver.find_element(By.CSS_SELECTOR, "button[type='submit'][name='addfriend']").click()
     time.sleep(3)
     print("[PASSED] - Add friend button successful.")
-    
+   
     friend = driver.find_element(By.CSS_SELECTOR, "a[href='/friend/goose']")
     if friend:
         print("[PASSED] - Friend Exists.")
     else:
         print("[FAILED] - Friend not found.")
 
+
     dark_mode_button = driver.find_element(By.ID, "darkModeToggle")
     if dark_mode_button:
         print("[PASSED] - Dark Mode Button Exists.")
     else:
         print("[FAILED] - Dark Mode button not found.")
+
 
     textarea = driver.find_element(By.NAME, "post")
     textarea.click()
@@ -74,7 +94,13 @@ try:
     print("[PASSED] - Log out successful.")
 
 
-    
+
+
+   
+
+
+
+
 
 
 
@@ -82,6 +108,10 @@ try:
 except Exception as e:
     print("Error:", e)
 
+
 finally:
     print("--= Ending Tests =--")
     driver.quit()
+
+
+
